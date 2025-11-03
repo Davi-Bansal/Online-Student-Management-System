a67eca82b90d27f55194848732ab994fe7d09e2d
Davi-Bansal <davybansal714@gmail.com>
2025-11-03 12:52:51 +0530

    Create CourseRepository.java

---
+++ CourseRepository.java
@@ -0,0 +1,9 @@
+package com.courseapp.repository;
+
+import com.courseapp.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Integer> {
+}
